54b65ebec4e9c5945e1d78686cc795d232752d9f
jmscasso <jmscasso@gmail.com>
2019-07-19 16:37:57 -0300

    1

--- app/src/main/java/io/github/jayemes/ardemo/AugmentedImageNode.java
+++ app/src/main/java/io/github/jayemes/ardemo/AugmentedImageNode.java
@@ -1,5 +1,6 @@
 package io.github.jayemes.ardemo;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.Log;
@@ -10,7 +11,6 @@ import com.google.ar.core.AugmentedImage;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Node;
-import com.google.ar.sceneform.SkeletonNode;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Material;
@@ -23,11 +23,14 @@ import java.util.concurrent.CompletableFuture;
 class AugmentedImageNode extends AnchorNode {
 
     private static final String TAG = "AugmentedImageNode";
+    public static final float OBJECTS_SCALE = 1.2f;
+
     private AugmentedImage image;
-    private static CompletableFuture<ModelRenderable> tankCF, waterCF, assetsCF;
+    private static CompletableFuture<ModelRenderable> tankCF, waterCF, handleCF;
     private static CompletableFuture<ViewRenderable> menuCF;
 
-    private Node tankNode, waterNode;
+    private ValueAnimator handleAnimator;
+    private Node tankNode, waterNode, handleNode;
     private HeatedTankODE tankODE;
     private Context context;
 
@@ -37,23 +40,26 @@ class AugmentedImageNode extends AnchorNode {
 
         if (tankCF == null) {
             tankCF = ModelRenderable.builder()
-                    .setSource(context, R.raw.tank)
+                    .setSource(context, R.raw.new_tank)
                     .build();
         }
 
         if (waterCF == null) {
             waterCF = ModelRenderable.builder()
-                    .setSource(context, R.raw.water)
+                    .setSource(context, R.raw.new_water)
                     .build();
         }
 
-        if (assetsCF == null) {
-            assetsCF = ModelRenderable.builder()
-                    .setSource(context, R.raw.bones)
+        if (handleCF == null) {
+            handleCF = ModelRenderable.builder()
+                    .setSource(context, R.raw.new_handle)
                     .build();
         }
 
         View menuView = View.inflate(context, R.layout.floating_menu, null);
+
+        handleAnimator = ValueAnimator.ofInt(1, 360);
+        handleAnimator.setDuration(1000);
 
         SeekBar levelBar = menuView.findViewById(R.id.levelSeekBar);
         SeekBar tempBar = menuView.findViewById(R.id.tempSeekBar);
@@ -62,6 +68,8 @@ class AugmentedImageNode extends AnchorNode {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 tankODE.setkValve(i / 50f);
+                if (!handleAnimator.isRunning())
+                handleAnimator.start();
             }
 
             @Override
@@ -102,8 +110,8 @@ class AugmentedImageNode extends AnchorNode {
     void setImage(AugmentedImage image) {
         this.image = image;
 
-        if (!tankCF.isDone() || !menuCF.isDone() || !waterCF.isDone() || !assetsCF.isDone()) {
-            CompletableFuture.allOf(tankCF, menuCF, waterCF, assetsCF)
+        if (!tankCF.isDone() || !menuCF.isDone() || !waterCF.isDone() || !handleCF.isDone()) {
+            CompletableFuture.allOf(tankCF, menuCF, waterCF, handleCF)
                     .thenAccept(aVoid -> setImage(image))
                     .exceptionally(throwable -> {
                         Log.e(TAG, "Exception loading models", throwable);
@@ -113,27 +121,31 @@ class AugmentedImageNode extends AnchorNode {
 
         setAnchor(image.createAnchor(image.getCenterPose()));
 
+        Vector3 scaleVector = Vector3.one().scaled(OBJECTS_SCALE);
+
         tankNode = new Node();
         tankNode.setParent(this);
-        tankNode.setLocalScale(new Vector3(0.3f, 0.3f, 0.3f));
-        tankNode.setLocalPosition(new Vector3(0, 0, 0));
-        tankNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1, 0), 180));
+        tankNode.setLocalScale(scaleVector);
+        tankNode.setLocalPosition(Vector3.zero());
         tankNode.setRenderable(tankCF.getNow(null));
 
         waterNode = new Node();
         waterNode.setParent(this);
-        waterNode.setLocalScale(new Vector3(0.3f, 0.3f, 0.3f));
-        waterNode.setLocalPosition(new Vector3(0, 0, 0));
-        waterNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1, 0), 180));
+        waterNode.setLocalScale(scaleVector);
+        waterNode.setLocalPosition(Vector3.zero());
         waterNode.setRenderable(waterCF.getNow(null));
+
+        handleNode = new Node();
+        handleNode.setParent(this);
+        handleNode.setLocalScale(scaleVector);
+        handleNode.setLocalPosition(Vector3.zero());
+        handleNode.setRenderable(handleCF.getNow(null));
 
         Node menuNode = new Node();
         menuNode.setParent(this);
-        //menuNode.setLocalScale(new Vector3(0.5f, 0.5f, 1f));
         menuNode.setLocalPosition(new Vector3(0.4f, 0.2f, 0));
-        //menuNode.setLocalRotation(Quaternion.axisAngle(new Vector3(1, 0, 0), 0));
+        menuNode.setLocalRotation(Quaternion.axisAngle(Vector3.up(), 180));
         menuNode.setRenderable(menuCF.getNow(null));
-
 
     }
 
@@ -142,7 +154,10 @@ class AugmentedImageNode extends AnchorNode {
         super.onUpdate(frameTime);
 
         if (waterNode != null) {
-            waterNode.setLocalScale(new Vector3(.3f, (float) (tankODE.getH() / 12f), .3f));
+            waterNode.setLocalScale(new Vector3(
+                    OBJECTS_SCALE,
+                    (float) (tankODE.getH() / 3f * OBJECTS_SCALE),
+                    OBJECTS_SCALE));
 
             float temp = (float) tankODE.getTOut();
 
@@ -156,7 +171,14 @@ class AugmentedImageNode extends AnchorNode {
                 materialCompletableFuture.thenAccept(
                         mat -> waterNode.getRenderable().setMaterial(mat));
             }
+        }
 
+        if (handleNode != null) {
+            if (handleAnimator.isRunning()) {
+                handleNode.setLocalRotation(Quaternion.axisAngle(
+                        Vector3.back(),
+                        (int) handleAnimator.getAnimatedValue()));
+            }
         }
 
     }
